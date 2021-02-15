38128428247aaf792aabbd03753ba302b165a192
Bert Witzel <bwitzel82@gmail.com>
2021-02-15 17:17:58 -0500

    Get API to work

--- js/app.js
+++ js/app.js
@@ -1,41 +1,31 @@
 const employeesGrid = document.getElementById('employees');
 
-generateCard = data => {
-    const section = document.createElement('section');
-    section.className = "employee-card";
-    employeesGrid.appendChild(section);
-    section.innerHTML = `
-        <div>
-            <img src="${data.picture.large}">
-        </div>
-        <div>
-            <h2>${data.name.first} ${data.name.last}</h2>
-            <p>${data.email}</p>
-            <p>${data.location.city}</p>
-        </div>
-    `;
+function generateCard(data){
+    data.forEach( person => {
+        const section = document.createElement('section');
+        section.className = "employee-card";
+        employeesGrid.appendChild(section);
+        section.innerHTML = `
+            <div>
+                <img src="${person.picture.large}">
+            </div>
+            <div>
+                <h2>${person.name.first} ${person.name.last}</h2>
+                <p>${person.email}</p>
+                <p>${person.location.city}</p>
+            </div>
+        `;
+    })
 }
 
-function statusCheck(response){
-    if(response.ok){
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(new Error(response.statusText));
-    }
-}
+fetch('https://randomuser.me/api/?results=12')
+    .then(response => response.json())
+    .then(data => generateCard(data.results))
+    .catch( err => {
+        employeesGrid.innerHTML = '<h3>Something went wrong!</h3>';
+        console.log(err);
+      })
 
-function fetchData(url) {
-    return fetch(url)
-        .then(statusCheck)
-        .then(response => response.json())
-        .then(data => generateCard(data.results[0]))
-}
-
-for (let i = 0; i < 12; i++) {
-    fetchData('https://randomuser.me/api/');
-}
-
-fetchData('https://randomuser.me/api/?results=12');
 
 
 
